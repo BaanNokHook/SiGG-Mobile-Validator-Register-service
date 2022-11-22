5491cab665052e77a8ebf64a1c84f85fcb071479
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 13:47:11 +0700

    SiGG-Mobile-Validator-Register-service-database-postgres.go

--- database/postgres.go
+++ database/postgres.go
@@ -15,7 +15,7 @@ import (
 var instance *Postgres
 
 /*
- * User and Org business layer to accept request from service layer and persist in database.
+ *	User and Org business layer to accept request from service layer and persist in database.
 **/
 
 type Postgres struct {
@@ -31,12 +31,12 @@ func NewPG() *Postgres {
 }
 
 func (pg *Postgres) autoMigrate() {
-	pg.Db.autoMigrate(&model.user{})
+	pg.Db.AutoMigrate(&model.User{})
 	pg.Db.AutoMigrate(&model.Org{})
 	fmt.Println("Automigrate complete")
 }
 
-func (pg *Postgres) Connect(e env.provider) error {
+func (pg *Postgres) Connect(e env.Provider) error {
 	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Kolkata",
 		e.Get("DB.HOST"), e.Get("DB.USERNAME"), e.Get("DB.PASSWORD"), e.Get("DB.DATABASE"), e.Get("DB.PORT"))
 
@@ -61,16 +61,17 @@ func (pg *Postgres) CreateUser(user *model.User) (*model.User, error) {
 }
 
 func (pg *Postgres) GetUserById(id string) (*model.User, error) {
-	if e := pg.Db.Create(&user).Error; e != nil {
+	var user *model.User
+	if e := pg.Db.First(&user, "id = ?", id).Error; e != nil {
 		return nil, e
 	}
-
+	user.Password = ""
 	return user, nil
 }
 
-func (pg *Postgres) GetUserByName(name string) (*model.user, error) {
+func (pg *Postgres) GetUserByName(name string) (*model.User, error) {
 	var user *model.User
-	if e := pg.Db.Where(&model.user{Name: name}).First(&user).Error; e != nil {
+	if e := pg.Db.Where(&model.User{Name: name}).First(&user).Error; e != nil {
 		return nil, e
 	}
 	return user, nil
@@ -88,6 +89,8 @@ func (pg *Postgres) CreateOrg(org *model.Org) (*model.Org, error) {
 	if e := pg.Db.Create(&org).Error; e != nil {
 		return nil, e
 	}
+
+	return org, nil
 }
 
 func (pg *Postgres) GetOrgById(id string) (*model.Org, error) {
@@ -121,7 +124,7 @@ func (pg *Postgres) Signup(u *model.Signup) (*model.User, error) {
 			return nil, e
 		}
 
-		u.user.OrgID = org.Base.ID
+		u.User.OrgID = org.Base.ID
 		if user, e = pg.CreateUser(&u.User); e != nil {
 			tx.Rollback()
 			return nil, e
